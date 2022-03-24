59198c997333a58538fca3a4d42a8542647138fb
Leo Silva <devleonardosilva@gmail.com>
2022-03-24 12:41:42 -0300

    STYLE: Navegação header criada

---
+++ src/components/Header/Menu/index.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import styled, { css } from "styled-components";
+
+import { Link } from "react-router-dom";
+
+const Nav = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    background-color: ${theme.colors.black};
+    width: 100%;
+    color: ${theme.colors.white};
+  `}
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 80%;
+  padding-left: 20rem; // espaço lado trocar
+`;
+
+
+const Li = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  a{
+    list-style-type: none;
+    text-decoration: none;
+    color: white;
+    font-size: 0.9rem;
+  }                             // add hover futuramente e detalhes
+`;
+
+const Menu = () => (
+  <Nav>
+    <NavMenu>
+      <Li>
+        <Link to="/">
+          Home
+        </Link>
+      </Li>
+      <Li>
+        <Link to="/contact">
+          Contact
+        </Link>
+      </Li>
+      <Li>
+        <Link to="/about">
+          About
+        </Link>
+      </Li>
+    </NavMenu>
+  </Nav>
+);
+
+export default Menu;
