a9e96f286806132871129b179f1f2f7490dc7ca1
Leo Silva <devleonardosilva@gmail.com>
2022-03-24 12:40:24 -0300

    FEAT: add rotas das pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import GlobalStyle from "./styles/global-style";
-import { BrowserRouter } from "react-router-dom";
-import Theme from '../src/styles/theme';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Theme from "../src/styles/theme";
 
-// import Routes from "./routes";
+import Layout from "./components/Layuot/layout";
+import CreateLogin from './pages/CreateLogin';
+import Painel from './pages/Painel';
+import NewLead from './pages/NewLead';
 
 function App() {
-
   return (
     <BrowserRouter>
-        <Theme>
-
-
-        </Theme>  
-      <GlobalStyle/>
+      <Theme>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<CreateLogin />} />
+            <Route path="/Painel" element={<Painel />} />
+            <Route path="/NewLead" element={<NewLead />} />
+          </Routes>
+        </Layout>
+      </Theme>
+      <GlobalStyle />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
