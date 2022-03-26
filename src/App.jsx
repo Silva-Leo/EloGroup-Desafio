0cbd990309974e324e74def201b2e04fe2e35149
Leo Silva <devleonardosilva@gmail.com>
2022-03-26 18:49:39 -0300

    FEAT: criação page PainelAddLead

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Theme from "../src/view/styles/theme";
 import Layout from "../src/view/assets/components/Layuot/layout";
 import CreateLogin from '../src/view/pages/CreateLogin';
 import Painel from '../src/view/pages/Painel';
+import PainelAddLead from "./view/pages/PainelAddLead";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<CreateLogin />} />
             <Route path="/Painel" element={<Painel />} />
+            <Route path="/Painel/NewLead" element={<PainelAddLead />} />
           </Routes>
         </Layout>
       </Theme>
---
+++ src/view/pages/PainelAddLead.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import NewLead from "../assets/components/Modal/index";
+
+export default function PainelAddLead() {
+  return (
+    <>
+      <NewLead />
+    </>
+  );
+}
